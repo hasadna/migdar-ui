1978bc2f0cea899c895012731986d4a33dbe9e62
Adam Kariv <adam.kariv@gmail.com>
2018-12-06 22:32:19 +0200

    More home page implementation

--- .travis.sh
+++ .travis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DOCKER_IMAGE=akariv/migdar-ui
+DOCKER_IMAGE=hasadna/migdar-ui
 
 if [ "${1}" == "install" ]; then
     curl -L https://raw.githubusercontent.com/OriHoch/travis-ci-operator/master/travis_ci_operator.sh > $HOME/bin/travis_ci_operator.sh &&\
