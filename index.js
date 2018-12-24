a1d3d0d25c687d33c584b40f18a9fec8619d19f2
Adam Kariv <adam.kariv@gmail.com>
2018-12-24 12:31:56 +0200

    Fix index.js to serve paths properly

--- index.js
+++ index.js
@@ -2,9 +2,18 @@
 
 const express = require('express');
 const app = express();
+const rootDir = './dist/migdar-ui/';
 
 app.set('port', process.env.PORT || 8000);
-app.use(express.static('dist/migdar-ui/'))
+app.use('/', express.static(rootDir, {
+  index: false,
+  maxAge: '1d',
+}));
+app.route('/*')
+    .get(function(req, res) {
+        res.sendFile('index.html', {root: rootDir});
+    });
+
 
 app.listen(app.get('port'), function() {
   console.log('Listening port ' + app.get('port'));
