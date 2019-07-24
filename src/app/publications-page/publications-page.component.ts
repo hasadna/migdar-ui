367808dd1cc4eac8dc99be2c2e79cbc5c981546b
Adam Kariv <adam.kariv@gmail.com>
2019-07-24 10:53:29 +0300

    new header

--- src/app/publications-page/publications-page.component.ts
+++ src/app/publications-page/publications-page.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ApiService } from '../api.service';
 import { SearchManager } from '../search-manager';
+import { HeaderStateService } from '../header-state.service';
 
 @Component({
   selector: 'app-publications-page',
   templateUrl: './publications-page.component.html',
   styleUrls: ['./publications-page.component.less']
 })
-export class PublicationsPageComponent implements OnInit {
+export class PublicationsPageComponent implements OnInit, OnDestroy {
 
   search: SearchManager;
 
-  constructor(public api: ApiService) {
+  constructor(public api: ApiService, private header: HeaderStateService) {
     this.search = new SearchManager(api);
     this.search.search(null, 'publications');
   }
 
   ngOnInit() {
+    this.header.section = 'publications';
+  }
+
+  ngOnDestroy() {
+    this.header.clear();
   }
 
 }
