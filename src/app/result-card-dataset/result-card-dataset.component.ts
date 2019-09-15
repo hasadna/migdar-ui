d6e2b91c1f626a4c29fc28d11890171bc938585c
Adam Kariv <adam.kariv@gmail.com>
2019-09-15 22:49:08 +0300

    Fixing links in all the site

--- src/app/result-card-dataset/result-card-dataset.component.ts
+++ src/app/result-card-dataset/result-card-dataset.component.ts
@@ -8,14 +8,25 @@ import { colorScale as cs } from '../constants';
   templateUrl: './result-card-dataset.component.html',
   styleUrls: ['./result-card-dataset.component.less']
 })
-export class ResultCardDatasetComponent {
+export class ResultCardDatasetComponent implements OnChanges{
 
   @Input() result: any;
   @Input() genderIndex: boolean;
   @Input() large: boolean;
 
   colorScale = cs;
+  _series = null;
 
   constructor(public _: I18nService) { }
+
+  ngOnChanges() {
+    if (this.result) {
+      window.setTimeout(() => {
+        this._series = this.result.series;
+      }, 100);
+    } else {
+      this._series = null;
+    }
+  }
 
 }
